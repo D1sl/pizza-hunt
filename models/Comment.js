1f65cf73246a15d3df2e545010588c0d4ba368c9
D1sl <94911312+D1sl@users.noreply.github.com>
2022-04-12 13:36:16 -0500

    18.3.2

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,16 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-      }
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Comment = model('Comment', CommentSchema);
