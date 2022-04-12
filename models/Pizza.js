2746ab8d9e499892a6f02d318628c014bcdf3a09
D1sl <94911312+D1sl@users.noreply.github.com>
2022-04-12 13:28:26 -0500

    18.2.8

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,23 +1,44 @@
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
-  },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  },
-  toppings: []
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
+    },
+    {
+        toJson: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 
+// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
+// export the Pizza model
 module.exports = Pizza;
